ec8808783230c5adaa2665b6508ad788ecfb5000
Štěpán Stenchlák <s.stenchlak@gmail.com>
2023-10-03 20:50:35 +0200

    fix endpoint

--- model.js
+++ model.js
@@ -6,7 +6,7 @@ const MM_API_ENDPOINT = "https://demo.mmcatdb.com/api/v1/public/schema-categorie
 
 // Change this to use multiple independent instances under the same DS backend
 // Used as a key to store MM project ID in DS
-const DATASPECER_MM_METADATA = "https://demo.mm-evocat.com/mmcat/dataspecer-self-metadata";
+const DATASPECER_MM_METADATA = "https://demo.mmcatdb.com/mmcat/dataspecer-self-metadata";
 
 
 const CREATE_SPECIFICATION_PAYLOAD = {
